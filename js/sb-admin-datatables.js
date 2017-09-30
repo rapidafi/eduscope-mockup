fcec297884ea772616644030cdd6df02bccc9a8d
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-09-30 12:28:25 +0300

    Clearing up for more. First setup of DataTables.

--- js/sb-admin-datatables.js
+++ js/sb-admin-datatables.js
@@ -1,4 +1,25 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  /*
+  $.getJSON( "https://sa.rapida.fi/eduscope.php/eduscope_education/korkeakoulu=Saimaan%20ammattikorkeakoulu", function( data ) {
+    $("#dataTable").append("<tbody>");
+    $.each( data, function( key, val ) {
+      $("#dataTable").append("<tr><td>"+val.vuosi+"</td><td>"+val.tutkinnot+"</td><td>"+val.tutkinnot_pros+"</td></tr>");
+    });
+    $("#dataTable").append("</tbody>");
+  });
+  //*/
+  $('#dataTable').DataTable({
+    "ajax": {
+      "url": "https://sa.rapida.fi/eduscope.php/eduscope_education/korkeakoulu=Saimaan%20ammattikorkeakoulu",
+      "dataSrc": "", //there's no data-named object
+    },
+    "columns": [
+      { "data": "vuosi" },
+      { "data": "tutkinnot" },
+      { "data": "tutkinnot_pros" }
+    ],
+    "order": [[0,"desc"]]
+  });
+  //*/
 });
