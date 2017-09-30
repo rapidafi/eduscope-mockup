acb5a5e6312df938d76351d2246ba65359f9084c
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-09-30 13:09:06 +0300

    Tidying

--- js/sb-admin-datatables.js
+++ js/sb-admin-datatables.js
@@ -1,14 +1,5 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  /*
-  $.getJSON( "https://sa.rapida.fi/eduscope.php/eduscope_education/korkeakoulu=Saimaan%20ammattikorkeakoulu", function( data ) {
-    $("#dataTable").append("<tbody>");
-    $.each( data, function( key, val ) {
-      $("#dataTable").append("<tr><td>"+val.vuosi+"</td><td>"+val.tutkinnot+"</td><td>"+val.tutkinnot_pros+"</td></tr>");
-    });
-    $("#dataTable").append("</tbody>");
-  });
-  //*/
   $('#dataTable').DataTable({
     "ajax": {
       "url": "https://sa.rapida.fi/eduscope.php/eduscope_education/korkeakoulu=Saimaan%20ammattikorkeakoulu",
@@ -21,5 +12,4 @@ $(document).ready(function() {
     ],
     "order": [[0,"desc"]]
   });
-  //*/
 });
